bb5cdbb8e93c0638ecf226fec6ab151752b41053
paolofraz <paolofraz@gmail.com>
2023-02-16 18:22:31 +0100

    Using pathlib and improved regex

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Automatically correcting DL homeworks
 
 To extract the answers, place jupyter notebooks in `ipynb/` and then run the `extract.sh` script. A csv with automatically extracted answers for all notebooks will be created in `results/`.
+These answers have to be delimited by the strings `%START` and `%END`.
--- utils/auto_finder.py
+++ utils/auto_finder.py
@@ -1,41 +1,47 @@
 import re
-import os
+from pathlib import Path
+
 import pandas as pd
 
 
-def main():
-  py_files_path = os.path.join(os. getcwd(), 'py')
-  files = list(filter(lambda x:".py" in x, os.listdir(py_files_path)))
-
-  if os.path.exists("results/results.csv"):
-    results = pd.read_csv("results/results.csv")
-  else:
-    results = pd.DataFrame()
-
-  for f_idx, f in enumerate(files):
-    name = f.split("/")[-1].split(".")[0]
-    answers = get_with_re(os.path.join(py_files_path, f))
-    columns = ["Q"+str(n_col+1) for n_col in range(len(answers))]
-
-    ans_df = pd.DataFrame({
-      col: ans.replace("%START", "").replace("%END", "")
-      for col, ans in zip(columns, answers)
-  }, index=[name])
-    results = results.append(ans_df)
-  results.to_csv("results/results.csv") #Rewirting
-
-
 def get_with_re(path):
-  answers = []
-  with open(path, 'r') as f:
-    hw = f.read()
-    Match = re.findall(r'%START.*?%END', hw, flags=re.DOTALL)
-    seperator = '\n'+'-'*25 + '\n'
-    answer = seperator
-    for ans in Match:
-      answers += [ans]
-    return answers
+    answers = []
+    with open(path, 'r') as f:
+        hw = f.read()
+        match = re.findall(r"(?<=%START)(.*?)(?=%END)", hw, flags=re.DOTALL)
+        # What's the point of this? I don't get it
+        # seperator = '\n' + '-' * 25 + '\n'
+        # answer = seperator
+        for ans in match:
+            ans = ans.replace('\n', '').replace('\r', '').replace('#', '').replace('*', '')
+            answers += [ans]
+        return answers
+
+
+def main():
+    # Get proper paths
+    project_path = Path(__file__).parent.parent
+    py_files_path = (project_path / "py").resolve()
+    py_files_path.mkdir(exist_ok=True, parents=True)
+    py_files = sorted(py_files_path.glob('*.py'))
+
+    # Store results
+    results_path = (project_path / "results/results.csv").resolve()
+    if results_path.exists():
+        results = pd.read_csv(results_path)
+    else:
+        results = pd.DataFrame()
+
+    for f in py_files:
+        name = f.stem
+        answers = get_with_re(f)
+        columns = ["Q" + str(n_col + 1) for n_col in range(len(answers))]
+
+        ans_df = pd.DataFrame({col: ans for col, ans in zip(columns, answers)}, index=[name])
+        results = pd.concat([results, ans_df])
+    results.to_csv(results_path)  # Rewriting
 
 
 if __name__ == '__main__':
-  main()
+    main()
+    print("All finished!")
